b6495d0365654208429d8689d86730c50d1b7f15
samsar-4 <samsurfing04@gmail.com>
2026-08-07 06:31:15 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"//newer version of js bas use hoga waise ab yeh default hi rehta hai toh jaruri nahi likhna hai
+//alert(3+3) we are using nodejs not browser toh chalegaa nahi
+console.log("samay")
+/* 
+number => 2 to power 53
+bigInt => bigger numbers ka jarurat pade toh bigger website like reddit etc
+string => "samay is changed now"
+null => single standalone value typeof null => object
+undefined => value not given typeof undefined => undefined
+boolean => true/false 
+*/
+var age =800
+console.log(typeof("samay"))
+console.table([typeof(null),typeof(undefined),typeof(true),typeof(1981),typeof("samay"),age])
